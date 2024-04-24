d2754132098e760235aff6f2cf97924fe11c6bdc
Nick <iamnicolus@gmail.com>
2024-04-24 15:05:05 +0700

    Group Files -1

--- App.js
+++ App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { View, Button, StyleSheet, StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import HomeCard from './app/HomeCard.js';
+import HomeCard from './app/Card/HomeCard.js';
 import ProfileScreen from './app/ProfileScreen.js';
+import EnrollCard from './app/Card/EnrollCard.js';
 
 export default function App() {
   return (
-      <ProfileScreen />
+      <EnrollCard />
   );
 }
---
+++ app/Card/DormCard.js
@@ -0,0 +1,84 @@
+import React from 'react'
+import { View, Text, StyleSheet, TouchableOpacity, ScrollView, StatusBar, Image } from 'react-native';
+
+const DormCard = () => {
+    return (
+        <ScrollView contentContainerStyle={styles.scrollViewContent}>
+            <View style={styles.container}>
+                <View style={styles.coursesBox}>
+                    <TouchableOpacity style={styles.coursesItem}>
+                        <Image source={require('./img/enroll.png')} style={styles.itemImage} />
+                        <View style={[styles.itemBackground, { backgroundColor: '#fffbda' }]} />
+                        <Text style={styles.itemTitle}>Dorm Booking</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.coursesItem}>
+                        <Image source={require('./img/health.png')} style={styles.itemImage} />
+                        <View style={[styles.itemBackground, { backgroundColor: '#ffcdea' }]} />
+                        <Text style={styles.itemTitle}>Service</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        </ScrollView>
+    )
+}
+
+const styles = StyleSheet.create({
+    scrollViewContent: {
+        flexGrow: 1,
+        marginTop: StatusBar.currentHeight,
+    },
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        backgroundColor: '#fff',
+        minHeight: '100%',
+    },
+    coursesBox: {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        paddingVertical: 50,
+        justifyContent: 'center',
+        marginBottom: 20,
+    },
+    coursesItem: {
+        width: '65%',
+        marginHorizontal: 15,
+        marginBottom: 30,
+        overflow: 'hidden',
+        borderRadius: 15,
+        backgroundColor: '#f3f3f3',
+        borderColor: '#000',
+    },
+    itemImage: {
+        overflow: 'hidden',
+        width: '70%',
+        height: '70%',
+        position: 'absolute',
+        marginLeft: 140,
+        marginTop: 50,
+        zIndex: 4,
+    },
+    itemBackground: {
+        height: 128,
+        width: 128,
+        borderRadius: 64,
+        position: 'absolute',
+        top: -75,
+        right: -75,
+        zIndex: 1,
+        transform: [{ scale: 100 }],
+    },
+    itemTitle: {
+        minHeight: 87,
+        marginBottom: 25,
+        paddingTop: 20,
+        paddingLeft: 10,
+        fontWeight: 'bold',
+        fontSize: 30,
+        color: '#000',
+        zIndex: 5,
+        position: 'relative',
+    },
+});
+
+export default DormCard
