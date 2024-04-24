cffe5d12bee485c2fcedd33ccdf17f8b5bc1ccca
Nick <iamnicolus@gmail.com>
2024-04-24 22:18:54 +0700

    Draft Feature

--- App.js
+++ App.js
@@ -2,13 +2,10 @@ import React from 'react';
 import { View, Button, StyleSheet, StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import BookBooking from './app/Books/BookBooking.js'
-import EnrollCard from './app/Card/EnrollCard.js';
-import Schedule from './app/Study/Schedule.js';
-import Transcript from './app/Study/Transcipt.js';
+import HomeCard from './app/Card/HomeCard';
 
 export default function App() {
   return (
-      <Transcript />
+    <HomeCard />
   );
 }
