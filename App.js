686336bdd4ace7fd2fb64fdb8de4b2f0592d38aa
Nick <iamnicolus@gmail.com>
2024-04-24 23:09:29 +0700

    Install Dependencies

--- App.js
+++ App.js
@@ -3,9 +3,21 @@ import { View, Button, StyleSheet, StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeCard from './app/Card/HomeCard';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Enroll from './app/Study/Enroll';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <HomeCard />
+    <NavigationContainer>
+      <Stack.navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeCard}
+        />
+          <Stack.Screen name='Enroll' component={Enroll} />
+      </Stack.navigator>
+    </NavigationContainer>
   );
 }
